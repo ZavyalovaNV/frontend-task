1ebdb4c00a36497427c949dca2665e9eafdb7138
Nataliya Zavyalova <zav.nataliya@mail.ru>
2020-02-26 02:34:47 +0400

    Доработки

--- clean-water/src/components/order-water-list/order-water-list.js
+++ clean-water/src/components/order-water-list/order-water-list.js
@@ -9,13 +9,13 @@ export default class OrderWaterList extends React.Component {
             <h2>Вода</h2>
             <ul className="bottle-list">
                 <li>
-                    <OrderWater volume="12.8" price="220" count="0" />
+                    <OrderWater volume="12.8" price="220" count="0" selected="1" />
                 </li>
                 <li>
-                    <OrderWater volume="1.5" price="175" count="0" />
+                    <OrderWater volume="1.5" price="175" count="0" selected="" />
                 </li>
                 <li>
-                    <OrderWater volume="0.5" price="270" count="0" />
+                    <OrderWater volume="0.5" price="270" count="0" selected="" />
                 </li>
             </ul>
         </div>
--- clean-water/src/components/order-water/order-water.js
+++ clean-water/src/components/order-water/order-water.js
@@ -2,31 +2,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './order-water.css';
 
-export default class OrderWater extends React.Component {
+class EditCountMenu extends React.Component {
     state = {
         count: this.props.count,
-        selected: true
+        selected: this.props.selected
     };
 
-    onToggleClick = (increment) => {
+    onIncrementMinus = () => {
         this.setState({
-                count: this.props.count,
-                selected: true
+                count: +this.state.count - 1,
+                selected: this.state.selected
+            })
+    }
+
+    onIncrementPlus = () => {
+        this.setState({
+                count: +this.state.count + 1,
+                selected: this.state.selected
             })
     }
     
     render() {
+        if (!this.state.selected) {
+            return null
+        }
+        return (
+            <div className="count-menu">
+                <button className="minus" onClick={this.onIncrementMinus}></button>
+                <span>{this.state.count}</span>
+                <button className="plus" onClick={this.onIncrementPlus}></button>
+            </div>
+        )
+    }
+}
+
+export default class OrderWater extends React.Component {
+    state = {
+        count: this.props.count,
+        selected: this.props.selected,
+        image: ''
+    };
+
+
+    onToggleClick = () => {
+        this.setState({
+                count: this.state.count,
+                selected: !this.state.selected
+            })
+    }
+    
+    render() {
+        console.log(this.state);
         const { volume, price } = this.props;
-        console.log(volume, price);
+        const { selected, count } = this.state;
+        console.log(selected, price);
 
         let classList = 'water-bottle';
-        classList = this.state.selected ? (classList + ' active') : classList;
+        classList = (!!selected) ? (classList + ' active') : classList;
+
+        let EditCountElement = () => <EditCountMenu count={count} selected={selected}/>;
 
         return (
             <div className={classList}>
-                <p className="bottle-volume">{volume} л</p> 
-                <p className="bottle-count">{this.state.count} шт</p> 
-                <p className="bottle-price">{price} ₽</p> 
+                <img src="../img/bg.png" alt=""></img>
+                <div className="water-bottle-wrapper" onClick={this.onToggleClick}>
+                    <p className="bottle-volume">{volume} л</p> 
+                    <p className="bottle-count">{count} шт</p> 
+                    <p className="bottle-price">{price} ₽</p> 
+                </div>
+                <EditCountElement />
             </div>)
     }
 }
